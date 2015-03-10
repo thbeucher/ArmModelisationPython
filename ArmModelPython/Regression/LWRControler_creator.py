5b2d74bcaf93c9f90919f9eca27b8ded6b43a632
thbeucher <beucherlamacque.thomas@gmail.com>
2015-03-10 17:27:03 +0100

    plot test

--- ArmModelPython/Regression/LWRControler_creator.py
+++ ArmModelPython/Regression/LWRControler_creator.py
@@ -1,7 +1,9 @@
 from FileProcessing.FileReading import *
 from FileProcessing.FileSaving import *
 from Regression.functionApproximator_LWR import *
-
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from mpl_toolkits.mplot3d import Axes3D
 
 #Lecture des fichiers de trajectoire
 fileR = FileReading()
@@ -24,11 +26,26 @@ for el in fileR.name_store:
 print("Fin du traitement!")
 
 #Plot
+print(fileR.uCommand["u1"])
+print(len(fileR.uCommand["u1"]))
+plt.figure()
+plt.xlim(-1, 1)
+plt.ylim(-1, 1)
+a = plt.plot(range(10),range(10),color='red')
+b = plt.plot(range(10),range(10),color='blue')
+plt.xlabel('t [s]')
+plt.ylabel('q1 [rad], q2[rad]')
+red_patch = mpatches.Patch(color='red', label='q1')
+blue_patch = mpatches.Patch(color='blue', label='q2')
+plt.legend([a, b])
+plt.title('Variations de q1 et q2 en fonction du temps')
+plt.grid(True)
+plt.show(block=True)
 
-
-
-
-    
+fig = plt.figure()
+ax = fig.add_subplot(111,projection='3d')
+ax.plot_wireframe([1,2,3,4,5], [0,2,4,8,9], [2,2.2,1.6,1.9,2])
+plt.show(block=True)
     
     
     
