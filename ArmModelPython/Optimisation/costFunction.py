costfunction computation for brent corrected

ArmModelPython/Optimisation/costFunction.py
@@ -44,6 +44,10 @@ class costFunction:
     def costFunctionJ(self, U, action, t):
         '''
         Cette fonction permet de calculer le cout d'une trajectoire en terme d'activation musculaire
+        
+        Entrees:    -U: Activations musculaires (tableau, numpy 1D array)
+                    -action: si le bras a atteint la cible action = 2 sinon action = 1 (scalar)
+                    -t: pas de temps utilise (scalar)
         '''
         usquare = np.square(U)
         usum = 0
@@ -52,7 +56,7 @@ class costFunction:
         mvtCost = (sqrt(usum))**2
         if action == 1:
             imReward = 0
-        else:
+        elif action == 2:
             imReward = 1
         self.Ju += np.exp(-t/self.gamma)*(self.rho*imReward - self.ups*mvtCost)
         
@@ -319,5 +323,5 @@ class costFunction:
         return JuCfMean
         
         
-        
-        
+    
+

ArmModelPython/Script/CostEvaluationForBrentBVPSolverTrajectories.py
@@ -28,9 +28,9 @@ def costEvalBrent():
             q = np.array([[el[2]], [el[3]]])
             coordEL, coordHA = mgd(q, robot.l1, robot.l2)
             if(coordHA[0] == 0 and coordHA[1] == 0.6175):
-                cf.costFunctionJ(fileR.data_store[str("trajectoire" + str(i+1+a) + "_command")][j], 1, 0.002)
+                cf.costFunctionJ(fileR.data_store[str("trajectoire" + str(i+1+a) + "_command")][j], 2, 0.002)
             else:
-                cf.costFunctionJ(fileR.data_store[str("trajectoire" + str(i+1+a) + "_command")][j], 0, 0.002)
+                cf.costFunctionJ(fileR.data_store[str("trajectoire" + str(i+1+a) + "_command")][j], 1, 0.002)
         name = "trajectoires_cout/trajectoire" + str(i+1+a) + "_cout"
         trajC.append(cf.Ju)
         cf.Ju = 0