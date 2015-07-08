009146aff5533ee124321dcd451830d08036a839
thbeucher <beucherlamacque.thomas@gmail.com>
2015-07-08 14:25:33 +0200

    __init__ file added, update 14:25

---
+++ MotorControlModel/runScript.py
@@ -0,0 +1,14 @@
+'''
+Author: Thomas Beucher
+
+Module: runScript
+
+Description: main script to run what we want in the project
+'''
+import os
+import sys
+
+
+from Main.Main import runFW
+
+runFW()
