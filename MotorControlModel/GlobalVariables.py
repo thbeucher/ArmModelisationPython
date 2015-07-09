9e58a691a709ea35452a4abb313845aa9df38bd6
thbeucher <beucherlamacque.thomas@gmail.com>
2015-07-09 11:33:51 +0200

    Update 11:33

--- MotorControlModel/GlobalVariables.py
+++ MotorControlModel/GlobalVariables.py
@@ -11,9 +11,6 @@ Description: global variables used in the project
 import os
 
 pathWorkingDirectory = os.getcwd()
-'''if not os.path.isdir(pathWorkingDirectory):
-    usrName = os.getlogin()
-    pathWorkingDirectory = pathWorkingDirectory.replace('//', '')'''
 pathListForm = pathWorkingDirectory.split("/")
 pathDataFolder = pathWorkingDirectory.replace(pathListForm[len(pathListForm)-1], '') + "Data/"
 pathTrajectoriesFolder = pathDataFolder + "trajectoires/"
