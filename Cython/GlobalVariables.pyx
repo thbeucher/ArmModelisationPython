a08382bf6e0bc7252677cf083e0d6066dfe16b39
thbeucher <beucherlamacque.thomas@gmail.com>
2015-07-16 17:08:17 +0200

    Updt 17:08

---
+++ Cython/GlobalVariables.pyx
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Author: Thomas Beucher
+
+Module: GlobalVariables
+
+Description: global variables used in the project
+'''
+
+import os
+
+pathWorkingDirectory = os.getcwd()
+pathListForm = pathWorkingDirectory.split("/")
+pathDataFolder = pathWorkingDirectory.replace(pathListForm[len(pathListForm)-1], '') + "Data/"
+pathTrajectoriesFolder = pathDataFolder + "trajectoires/"
